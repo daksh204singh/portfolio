d30757ee2ef9bafa7aad20e6c34785db51edb439
Daksh Pratap Singh <dsingh23@ncsu.edu>
2025-07-29 10:15:01 -0400

    Update company name from PROJECT REMA to The Rematriation Project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
             <WorkCard 
               work={{
                 _id: "work-1",
-                company: "PROJECT REMA",
+                company: "The Rematriation Project",
                 role: "SOFTWARE ENGINEER",
                 location: "Remote",
                 duration: "May 2021 – Present",
